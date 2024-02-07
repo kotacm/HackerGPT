7fad66ed7644d592e8e2321812ef15ebd98c0a41
thehackergpt <144740362+thehackergpt@users.noreply.github.com>
2024-02-07 12:53:02 -0500

    Remove HackerGPT API (#202)
    * Remove HackerGPT API
    
    * Update HackerGPT Plus Plan

--- components/Payments/UpgradeToPremiumPopup.tsx
+++ components/Payments/UpgradeToPremiumPopup.tsx
@@ -260,18 +260,6 @@ const UpgradeToPremiumPopup: React.FC<Props> = ({
                           </div>
                         </div>
                         <div className="mb-2 flex items-center">
-                          <div className="icon-container mr-2">
-                            <IconCircleCheck
-                              color={'#43A047'}
-                              size={22}
-                              strokeWidth={2}
-                            />
-                          </div>
-                          <div className="text-container flex-1">
-                            <p className="overflow-hidden truncate whitespace-nowrap">
-                              Access to our HackerGPT API
-                            </p>
-                          </div>
                         </div>
                         <div className="mb-2 flex items-center">
                           <div className="icon-container mr-2">
